bcffa1cda49b9ded73418b8ad21b6a115156dbc0
Amanduttbhagat <bhagataman980@gmail.com>
2021-03-16 00:39:39 -0500

    added Java folder with one file Hello.java

---
+++ Java/CP/Hello.java
@@ -0,0 +1,7 @@
+public class World{
+  String say_Hello = "Hello World"
+  public static void main(String[] args){
+    World object = new World();
+    System.out.println(object.say_Hello);
+  }
+}
